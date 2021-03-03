56f0e4c57c6464178d40745354f768586746f378
DATBOI <alexsimons9999@gmail.com>
2021-03-03 05:52:46 -0600

    Added MVP CLI

--- dokitheme/__main__.py
+++ dokitheme/__main__.py
@@ -3,8 +3,38 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument(
         '-l',
-        "--list",
+        '--list',
         action='store_true',
-        help="list available themes"
+        help='list available themes'
 )
+parser.add_argument(
+        '-t',
+        '--set-theme',
+        default='Rem',
+        metavar='theme',
+        action='store',
+        help='name of theme to install'
+        )
+parser.add_argument(
+        '-f',
+        "--set-font",
+        action='store_true',
+        help="install official doki theme font"
+        )
+parser.add_argument(
+        '-uf',
+        '-rf',
+        "--remove-font",
+        "--uninstall-font",
+        action='store_true',
+        help="uninstalls official doki theme font"
+        )
+parser.add_argument(
+        '-u',
+        '-r',
+        '--remove',
+        '--uninstall',
+        action='store_true',
+        help='restores previous styling'
+        )
 arguments = parser.parse_args()
