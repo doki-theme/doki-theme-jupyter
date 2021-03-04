0b57d0709b6eebca7183d1ad8fc18c66f84e2c8f
DATBOI <alexsimons9999@gmail.com>
2021-03-04 17:13:01 -0600

    Able to suggest argument based on name

--- dokitheme/argument_suggestion.py
+++ dokitheme/argument_suggestion.py
@@ -3,14 +3,31 @@ from Levenshtein import distance
 
 
 def suggest_theme(unknown_theme):
-    keys = list(themes.keys())
-    closest_match = keys[0]
-    min_distance = distance(unknown_theme, closest_match)
+    theme_display_names = list(themes.keys())
+    return find_min(
+        unknown_theme,
+        theme_display_names[0],
+        theme_display_names,
+        [
+            lambda themeName: themeName,
+            lambda themeName: themeName.split(": ")[1]
+        ]
+    )
 
-    for theme_name in keys:
-        this_distance = distance(unknown_theme, theme_name)
-        if(this_distance <= min_distance):
-            min_distance = this_distance
-            closest_match = theme_name
+
+def find_min(
+    target,
+    provided_closest_match,
+    list_to_search,
+    candidate_decorators
+):
+    min_distance = distance(target, provided_closest_match)
+
+    for theme_name in list_to_search:
+        for decorator in candidate_decorators:
+            this_distance = distance(target, decorator(theme_name))
+            if(this_distance <= min_distance):
+                min_distance = this_distance
+                closest_match = theme_name
 
     return closest_match
