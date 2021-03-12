8c1c9442188879d3f91671c7b2db25c9cea35bf3
DATBOI <alexsimons9999@gmail.com>
2021-03-12 06:19:17 -0600

    Almost good

--- buildSrc/buildInstallThemes.sh
+++ buildSrc/buildInstallThemes.sh
@@ -2,4 +2,5 @@
 
 yarn buildThemes
 cd ..
-python3 dokitheme --set-theme "Franxx: Zero Two Dark"
+#python3 dokitheme --set-theme "Franxx: Zero Two Dark"
+python3 dokitheme --set-theme "OreGairu: Yukinoshita Yukino"
