3cafdb9f889f37961ad9f0fec6a2433eadf6fe0b
DATBOI <alexsimons9999@gmail.com>
2021-03-15 18:03:39 -0500

    able to run cli app

--- src/dokithemejupyter/__main__.py
+++ src/dokithemejupyter/__main__.py
@@ -1,13 +1,3 @@
-import dokithemejupyter.theme_manager as theme_manager
-from dokithemejupyter.arguments import parser
+from . import main
 
-arguments = parser.parse_args()
-
-if arguments.list:
-    theme_manager.list_themes()
-    exit(0)
-elif arguments.remove:
-    theme_manager.remove_theme()
-    exit(0)
-else:
-    exit(theme_manager.install_theme(arguments.set_theme))
+main()
