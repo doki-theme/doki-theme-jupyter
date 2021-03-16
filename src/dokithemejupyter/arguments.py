5819551ed7a65746594e543f16e3608ff3d036fc
DATBOI <alexsimons9999@gmail.com>
2021-03-16 05:24:56 -0500

    Able to remember current theme

--- src/dokithemejupyter/arguments.py
+++ src/dokithemejupyter/arguments.py
@@ -12,7 +12,6 @@ parser.add_argument(
 parser.add_argument(
     '-t',
     '--set-theme',
-    default=default_theme,
     metavar='theme',
     action='store',
     help='name of theme to install'
--- src/dokithemejupyter/theme_manager.py
+++ src/dokithemejupyter/theme_manager.py
@@ -6,10 +6,12 @@ from dokithemejupyter.theme_installer import install_theme_styles, open_file
 
 from dokithemejupyter.argument_suggestion import suggest_theme
 
+
 def display_current_version():
     if os.path.exists(version_file_path):
         with open_file(version_file_path, 'r') as current_version:
             print("Doki Theme: Jupyter Notebook v{}".format(current_version.readlines()[0]))
+
 
 def list_themes():
     print("Theme Names (include double quotes): \n   {}".format('\n   '.join(
@@ -18,13 +20,17 @@ def list_themes():
 
 
 def get_current_theme():
+    current_theme = read_current_theme()
+    return themes[current_theme]
+
+
+def read_current_theme():
     if os.path.exists(current_theme_path):
         with open_file(current_theme_path, 'r') as current_theme_file:
             current_theme = current_theme_file.readlines()[0]
     else:
         current_theme = default_theme
-
-    return themes[current_theme]
+    return current_theme
 
 
 def remove_theme():
@@ -35,10 +41,9 @@ def remove_theme():
 
 
 def install_theme(theme_parameter):
-    """
-    Finna install the selected theme
-    """
-    if theme_parameter not in themes:
+    if theme_parameter is None:
+        theme_parameter = read_current_theme()
+    elif theme_parameter not in themes:
         print(
             "Unknown Theme \"{}\", did you mean \"{}\" ?".format(
                 theme_parameter,
