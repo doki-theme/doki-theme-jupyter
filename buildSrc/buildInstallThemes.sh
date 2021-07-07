353d26d6d108086c5f531d3cfdb32de1773e7636
DATBOI <alexsimons9999@gmail.com>
2021-07-07 17:46:11 -0500

    v4 - NekoPara Release

--- buildSrc/buildInstallThemes.sh
+++ buildSrc/buildInstallThemes.sh
@@ -3,5 +3,5 @@
 yarn buildThemes
 cd ../src
 #python3 -m src/dokithemejupyter --set-theme "Franxx: Zero Two Dark"
-python3 -m dokithemejupyter --set-theme "KillLaKill: Ryuko"
+python3 -m dokithemejupyter --set-theme "KillLaKill: Ryuko" --sticker
 #python3 -m src/dokithemejupyter --set-theme "OreGairu: Yukinoshita Yukino"
